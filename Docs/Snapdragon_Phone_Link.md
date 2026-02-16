7c04f73f9378c293055ba8795ff388166e30438a
azjam798276 <azjam969+2806@gmail.com>
2026-02-16 20:03:12 +0800

    Architecture: Added Snapdragon Phone-Link and Display Toggle Logic

---
+++ Docs/Snapdragon_Phone_Link.md
@@ -0,0 +1,10 @@
+# Snapdragon + Pi 5 Dual-Brain Setup
+
+## Hardware Connectivity
+- **Primary:** Pi 5 connected to eDP Input 1.
+- **Secondary:** Framework Slot (USB-C) connected to eDP Input 2.
+- **Data Link:** USB-C cable between Phone and Pi 5 (using USB Ethernet/Gadget mode).
+
+## Software Logic
+- **Display Switching:** Use the 'Source' button on the AP2K1.71 or map a QMK macro to toggle inputs.
+- **Data Sharing:** Enable 'USB Tethering' on the Snapdragon phone to give the Pi 5 high-speed 5G internet.
--- Firmware/qmk_keymap/keymap.c
+++ Firmware/qmk_keymap/keymap.c
@@ -19,3 +19,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_tkl_ansi(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, SAFE_RANGE, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT)
 };
+// Macro to send a signal to the AP2K1.71 to switch HDMI sources
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_SCRN_SWAP:
+            if (record->event.pressed) {
+                // Signal to MOSFET or GPIO to toggle display source
+                writePinHigh(B5); 
+            }
+            return false;
+    }
+    return true;
+}
